ed5d51f32e845df8b0b25e6066c65c1ead3babc5
papistan <jpapisan@gmail.com>
2018-02-07 14:38:31 -0800

    Adds mongoclient to connect db to routes

--- app/routes/postit_routes.js
+++ app/routes/postit_routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, db) {
-	app.post('/notes', (req, res) => {
-		console.log(req.body)
+	app.post('/postits', (req, res) => {
+		console.log(req.body);
 		res.send('Hello dude')
 	})
 
