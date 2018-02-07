5a056aac69b5130fa07fe1ff9a1378571a38933a
papistan <jpapisan@gmail.com>
2018-02-07 15:01:45 -0800

    Adds post route complete and tested

--- app/routes/postit_routes.js
+++ app/routes/postit_routes.js
@@ -1,7 +1,16 @@
 module.exports = function(app, db) {
+	
+
+
 	app.post('/postits', (req, res) => {
-		console.log(req.body);
-		res.send('Hello dude')
+		const postit = { title: req.body.title, body: req.body.body };
+		db.collection('postits').insert(postit, (err, result) => {
+			if (err) {
+				res.send({ 'error': 'An error has occurred' });
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
 	})
 
 }
--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, (err, database) => { 
-	if(err) return console.log(err)
+	if (err) return console.log(err);
 	require('./app/routes')(app, database);
 
 	app.listen(port, () => {
 		console.log('We are live on ' + port)
-	})
+	});
 })
