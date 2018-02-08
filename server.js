a8eeeb088d90fb5e44462c5767fae968d6b9c3a3
papistan <jpapisan@gmail.com>
2018-02-08 09:55:03 -0800

    Update env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db');
+// const db = require('./config/db');
 
 const app = express();
 
@@ -9,7 +9,9 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => { 
+var url = process.env.MONGOLAB_URI;
+
+MongoClient.connect(url, (err, database) => { 
 	if (err) return console.log(err);
 	require('./app/routes')(app, database);
 
